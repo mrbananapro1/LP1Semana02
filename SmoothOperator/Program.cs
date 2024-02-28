27eba1f6f55fc777cd2860d13c84084425c015d8
Leonardo Henrique <leonardojose@netcabo.pt>
2024-02-28 11:42:30 +0000

    Operadores ++ e --

---
+++ SmoothOperator/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SmoothOperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Insere número inteiro não negativo");
+            string s = Console.ReadLine();
+            byte m = byte.Parse(s);
+
+            Console.WriteLine(--m);
+            Console.WriteLine(++m);
+
+        }
+    }
+}

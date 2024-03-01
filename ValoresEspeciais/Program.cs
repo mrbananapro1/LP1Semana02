4764ff9871851f91adf9d6872453d6628285bb87
Leonardo Henrique <leonardojose@netcabo.pt>
2024-03-01 00:01:27 +0000

    Valor máximo e mínimo int

--- ValoresEspeciais/Program.cs
+++ ValoresEspeciais/Program.cs
@@ -7,6 +7,8 @@ namespace ValoresEspeciais
         static void Main(string[] args)
         {
             Console.WriteLine($"byte: mínimo = {byte.MinValue}, máximo = {byte.MaxValue}");
+            Console.WriteLine($"int: mínimo = {int.MinValue}, máximo = {int.MaxValue}");
+            ã
         }
     }
 }

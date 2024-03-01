37611763637e05754b8162f83faf9e83c0e26773
Leonardo Henrique <leonardojose@netcabo.pt>
2024-03-01 16:19:44 +0000

    O User pode inserir quantas balas ele quer que a MagicGun tenha

---
+++ MagicGun/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MagicGun
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string n;
+
+            Console.WriteLine("Give me a number of bullets for the MagicGun");
+            n = Console.ReadLine();
+
+
+        }
+    }
+}
